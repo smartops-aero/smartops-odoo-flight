c5f6c34955076798b790e9af68d64f707cb146c2
Alexis Yushin <alexis@apexive.com>
2024-11-12 17:06:50 -0500

    flight_uom

---
+++ flight_uom/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Flight UOM",
+    "summary": """
+        Adds aviation-specific units of measurement""",
+    "description": """
+        This module adds aviation-specific units of measurement including:
+        - Nautical Miles (nm)
+        - Knots (kt)
+        - Kilometers per hour (kph)
+        - Feet per second (fps)
+    """,
+    "author": "Apexive Solutions LLC",
+    "website": "https://github.com/smartops-aero/flight",
+    "license": "LGPL-3",
+    "category": "Industries",
+    "version": "16.0.1.0.0",
+    "depends": [
+        "uom",
+    ],
+    "data": [
+        "data/product_uom_data.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
