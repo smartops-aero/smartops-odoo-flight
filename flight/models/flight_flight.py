051da75b5ffe84c2b8bb2670b03044aefe185862
Alexis Yushin <alexis@apexive.com>
2024-11-13 20:50:56 -0500

    set departure to previous arrival of the aircraft

--- flight/models/flight_flight.py
+++ flight/models/flight_flight.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FlightFlight(models.Model):
@@ -25,3 +25,21 @@ class FlightFlight(models.Model):
     def toggle_locked(self):
         self.ensure_one()
         self.write({"locked": not self.locked})
+
+    @api.onchange("aircraft_id")
+    def _onchange_aircraft_id(self):
+        """When aircraft changes and departure is empty, set departure to last arrival."""
+        if self.aircraft_id and not self.departure_id:
+            # Search for the last flight of this aircraft
+            last_flight = self.search(
+                [
+                    ("aircraft_id", "=", self.aircraft_id.id),
+                    ("date", "<", self.date),
+                    ("arrival_id", "!=", False),
+                ],
+                order="date desc, id desc",
+                limit=1,
+            )
+
+            if last_flight:
+                self.departure_id = last_flight.arrival_id
