2a24b4b78504717335a67ae83e018ecf25ce1b2d
adar2378 <saiful@apexive.com>
2024-12-13 00:36:57 +0600

    add uoms for the fields

--- flight_aircraft_specifications/models/flight_aircraft.py
+++ flight_aircraft_specifications/models/flight_aircraft.py
@@ -13,13 +13,45 @@ class FlightAircraft(models.Model):
 
     # Aircraft specifications
     passenger_capacity = fields.Integer("Passenger Capacity")
-    range_nm = fields.Integer("Range (Nautical Miles)")
-    cruise_speed = fields.Integer("Cruise Speed (Knots)")
-    cabin_length = fields.Float("Cabin Length (ft)")
-    cabin_width = fields.Float("Cabin Width (ft)")
-    cabin_height = fields.Float("Cabin Height (ft)")
-    luggage_capacity = fields.Integer("Luggage Capacity (cu ft)")
-    useful_load = fields.Integer(
-        "Useful Load (lb)", 
-        help="Maximum useful load in pounds"
-    )
+    
+    range_nm = fields.Float("Range", required=True)
+    range_uom_id = fields.Many2one('uom.uom', string='Range Unit', 
+        domain="[('category_id', '=', %(flight_uom.product_uom_categ_distance)d)]",
+        default=lambda self: self.env.ref('flight_uom.product_uom_nm'),
+        required=True)
+    
+    cruise_speed = fields.Float("Cruise Speed", required=True)
+    cruise_speed_uom_id = fields.Many2one('uom.uom', string='Speed Unit',
+        domain="[('category_id', '=', %(flight_uom.product_uom_categ_speed)d)]",
+        default=lambda self: self.env.ref('flight_uom.product_uom_kt'),
+        required=True)
+    
+    cabin_length = fields.Float("Cabin Length")
+    cabin_length_uom_id = fields.Many2one('uom.uom', string='Length Unit',
+        domain="[('category_id', '=', %(uom.product_category_unit)d)]",
+        default=lambda self: self.env.ref('uom.product_uom_foot'),
+        required=True)
+    
+    cabin_width = fields.Float("Cabin Width")
+    cabin_width_uom_id = fields.Many2one('uom.uom', string='Width Unit',
+        domain="[('category_id', '=', %(uom.product_category_unit)d)]",
+        default=lambda self: self.env.ref('uom.product_uom_foot'),
+        required=True)
+    
+    cabin_height = fields.Float("Cabin Height")
+    cabin_height_uom_id = fields.Many2one('uom.uom', string='Height Unit',
+        domain="[('category_id', '=', %(uom.product_category_unit)d)]",
+        default=lambda self: self.env.ref('uom.product_uom_foot'),
+        required=True)
+    
+    luggage_capacity = fields.Float("Luggage Capacity")
+    luggage_capacity_uom_id = fields.Many2one('uom.uom', string='Volume Unit',
+        domain="[('category_id', '=', %(uom.product_category_vol)d)]",
+        default=lambda self: self.env.ref('uom.product_uom_cubic_foot'),
+        required=True)
+    
+    useful_load = fields.Float("Useful Load")
+    useful_load_uom_id = fields.Many2one('uom.uom', string='Weight Unit',
+        domain="[('category_id', '=', %(uom.product_category_kgm)d)]",
+        default=lambda self: self.env.ref('uom.product_uom_lb'),
+        required=True)
