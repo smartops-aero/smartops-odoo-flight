7f97f909b27b05fa2a111dfcc76d926ce02bbf58
adar2378 <saiful@apexive.com>
2024-12-14 00:56:36 +0600

    add aircraft views

---
+++ flight_aircraft_spec/models/flight_aircraft.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class FlightAircraft(models.Model):
+    _inherit = 'flight.aircraft'
+
+    spec_ids = fields.One2many(
+        'flight.aircraft.spec',
+        'aircraft_id',
+        string='Specifications'
+    )
