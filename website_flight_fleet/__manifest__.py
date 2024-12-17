28b378a1ae36ba8de4922885a89c5cf34ce42a1c
adar2378 <saiful@apexive.com>
2024-12-17 17:12:19 +0600

    remove not needed files

--- website_flight_fleet/__manifest__.py
+++ website_flight_fleet/__manifest__.py
@@ -38,7 +38,6 @@
         "views/pages/page_aircraft_detail.xml",
         "views/flight_aircraft_views.xml",
         "views/website_flight_fleet_views.xml",
-        "views/website_flight_fleet_templates.xml",
     ],
     "assets": {
         "website.assets_wysiwyg": [
