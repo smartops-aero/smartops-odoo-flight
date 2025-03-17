d28902b0e6cf8f720506686cb309dbc959c8b2e9
adar2378 <saiful@apexive.com>
2025-03-18 01:03:27 +0600

    fix issues with base_import_pipeline

--- base_import_pipeline/models/base_import_pipeline.py
+++ base_import_pipeline/models/base_import_pipeline.py
@@ -11,7 +11,7 @@ class BaseImportPipeline(models.Model):
         selection=lambda self: self._selection_implementation(),
         required=True,
     )
-    model_id = fields.Many2one('ir.model', string='Target Model', required=True)
+    model_id = fields.Many2one('ir.model', string='Target Model', required=True, ondelete='cascade')
     active = fields.Boolean(default=True)
     result_ids = fields.One2many('base.import.pipeline.result', 'pipeline_id', string='Import Results')
     
