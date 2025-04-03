b30736817f63920c917f570f261acae2626be987
adar2378 <saiful@apexive.com>
2025-04-03 13:07:38 +0600

    add back flight_import related modules

---
+++ flight_import_pilotlog/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "Flight Import PilotLog",
+    "version": "1.0",
+    "category": "Flight",
+    "author": "Apexive Solutions LLC",
+    "website": "https://github.com/smartops-aero/flight",
+    "summary": "Base module for importing pilot log data",
+    "description": """
+Flight Import PilotLog
+======================
+Base module for importing pilot log data with flexible transformers.
+    """,
+    "depends": ["base", "base_import", "flight", "flight_pilotlog", "flight_event"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/flight_import_pilotlog_transformer_views.xml",
+        "views/menus.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "flight_import_pilotlog/static/src/js/import_action.js",
+        ],
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+    "images": [
+        "static/description/banner.jpeg",
+    ],
+}
