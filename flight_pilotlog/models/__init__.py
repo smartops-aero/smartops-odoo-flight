77a5c79422f612a0fa3f84b9a2df98b222612427
adar2378 <saiful@apexive.com>
2025-04-03 13:13:50 +0600

    temporarily remove flight_pilotlog

--- flight_pilotlog/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import flight_pilot_event
-from . import flight_pilot_time
-from . import flight_pilot_remark
-from . import flight_flight
--- flight_pilotlog/wizards/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import flight_pilot_log_wizard
