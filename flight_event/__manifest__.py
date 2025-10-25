63e9bbfcd623934a9ac9cc0429f82bf7c24d897a
adar2378 <saiful@apexive.com>
2025-10-25 06:20:07 +0600

    chore: bump version

--- flight_event/__manifest__.py
+++ flight_event/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/smartops-aero/flight",
     "license": "LGPL-3",
     "category": "Industries",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "depends": [
         "base",
         "flight",
