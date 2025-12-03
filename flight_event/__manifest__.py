642e640cebbb5e7a3a0fbe0d429e527b55bb0ba2
adar2378 <saiful@apexive.com>
2025-12-04 02:10:09 +0600

    chore(flight_event): bump version to 18.0.1.1.0 and add changelog

--- flight_event/__manifest__.py
+++ flight_event/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/smartops-aero/smartops-odoo-flight",
     "license": "LGPL-3",
     "category": "Industries",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.1.0",
     "depends": [
         "base",
         "flight",
