6ff835091bb9b99756e2516bf0896c2549c8a60b
adar2378 <saiful@apexive.com>
2026-01-10 01:37:42 +0600

    chore: bump version of flight module

--- flight/__manifest__.py
+++ flight/__manifest__.py
@@ -17,7 +17,7 @@
     "website": "https://github.com/smartops-aero/smartops-odoo-flight",
     "license": "LGPL-3",
     "category": "Industries",
-    "version": "18.0.1.3.0",
+    "version": "18.0.1.3.1",
     "images": [
         "static/description/banner.jpeg",
     ],
